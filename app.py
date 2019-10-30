25de9fd1226b0d033b8087252470351a3633ba7c
Kamycek <k.mazur8-2001@wp.pl>
2019-10-30 20:26:41 +0100

    course, exercise get route

--- app.py
+++ app.py
@@ -4,14 +4,41 @@ import os
 app = Flask(__name__)
 
 
+def list_dir(dire):
+    data = {}
+    files = os.listdir(dire)
+    for f in files:
+        item = f.split('-')
+        data[item[0]] = item[1]
+    return data
+
+
 @app.route('/course')
 def course():
-    return jsonify(os.listdir('./course'))
+    return jsonify(os.listdir('course'))
 
 
-@app.route('/exercises')
-def course():
-    return jsonify(os.listdir('./exercises'))
+@app.route('/course/<int:num>', methods=['GET', 'POST'])
+def subject(num):
+    if request.method == 'GET':
+        fname = list_dir('course')[str(num)]
+        with open('./course/{0}-{1}'.format(num, fname), 'r') as file:
+            data = file.read()
+        return data
+
+
+@app.route('/exercise')
+def exercises():
+    return jsonify(os.listdir('exercise'))
+
+
+@app.route('/exercise/<int:num>', methods=['GET', 'POST'])
+def exercise(num):
+    if request.method == 'GET':
+        fname = list_dir('exercise')[str(num)]
+        with open('./exercise/{0}-{1}'.format(num, fname), 'r') as file:
+            data = file.read()
+        return data
 
 
 if __name__ == '__main__':
