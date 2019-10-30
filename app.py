2826e6873a895bb04a140fa13907a10ff3f1d89e
Kamycek <k.mazur8-2001@wp.pl>
2019-10-30 19:15:05 +0100

    dir structure

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/course')
+def course():
+    return jsonify(os.listdir('./course'))
+
+@app.route('/exercises')
+def course():
+    return jsonify(os.listdir('./course'))
+
+
+if __name__ == '__main__':
+    app.run()
