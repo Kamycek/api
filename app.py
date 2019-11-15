b6ca1702a8c6ede4c5c9dfd6f9e2e94c0e9017d8
Kamycek <k.mazur8-2001@wp.pl>
2019-11-15 18:19:06 +0100

    add uploading files

--- app.py
+++ app.py
@@ -2,17 +2,14 @@ from flask import Flask, request, jsonify, render_template, redirect, json
 import os
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 1024
 
 
 def list_dir(dire):
-    """Zwraca zawartość pliku list.json we wskazanym folderze."""
+    """Zwraca zawartosc pliku list.json we wskazanym folderze jako liste"""
     f = open('content/{}/list.json'.format(dire), 'r')
     data = json.loads(f.read())
     return data
-
-
-def sort_files():
-    pass
 
 
 @app.route('/')
@@ -21,6 +18,7 @@ def index():
 
 
 @app.route('/panel', methods=['POST'])
+# TODO sprawdzenie poprawnosci argumentu list_dir
 def panel():
     return render_template('panel.html', subjects=list_dir(request.form['index']))
 
@@ -34,7 +32,7 @@ def content():
         raw_file_name = request.form['name']
         file_name = raw_file_name + '.md'
         files = os.listdir('content')
-        dir_name = files[int(request.form['index']) - 1]
+        dir_name = files[int(request.form['index']) - 1]  # indeks od 0 bo w html jest od 1
         file.save(os.path.join('content/{}'.format(dir_name), file_name))
         print(request.form['index'])
         return render_template('status.html', status="201 Created"), 201
