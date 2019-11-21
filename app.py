5f05f49512208995029ac5addb9984f3048c3c6e
Kamycek <35150840+Kamycek@users.noreply.github.com>
2019-11-21 12:37:39 +0100

    cors

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify, render_template, redirect, json
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['MAX_CONTENT_LENGTH'] = 1024
 
 # TODO Automatyczna naprawa list.json
